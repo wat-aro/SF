594a8d0ab2a3d9ee2e9bfe3da1b9e224aef032b7
wat-aro <kazutas1008@gmail.com>
2018-10-18 01:29:47 +0900

    Finish list_funs

--- Lists_J.v
+++ Lists_J.v
@@ -51,4 +51,97 @@ Module NatList.
   Proof.
     intros p. destruct p as (n, m). simpl. reflexivity.
   Qed.
-End NatList.
+
+  Inductive natlist : Type :=
+  | nil : natlist
+  | cons : nat -> natlist -> natlist.
+
+  Notation "x :: l" := (cons x l) (at level 60, right associativity).
+  Notation "[]" := nil.
+  Notation "[ x , .. , y ]" := (cons x .. (cons y nil) ..).
+
+  Fixpoint repeat (n count : nat) : natlist :=
+    match count with
+    | O => nil
+    | S count' => n :: (repeat n count')
+    end.
+
+  Fixpoint length (l : natlist) : nat :=
+    match l with
+    | nil => O
+    | h :: t => S (length t)
+    end.
+
+  Fixpoint app (l1 l2 : natlist) : natlist :=
+    match l1 with
+    | nil => l2
+    | h :: t => h :: (app t l2)
+    end.
+
+  Notation "x ++ y" := (app x y) (right associativity, at level 60).
+
+  Example test_app1 : [1, 2, 3] ++ [4, 5] = [1, 2, 3, 4, 5].
+  Proof. reflexivity. Qed.
+  Example test_app2 : nil ++ [4, 5] = [4, 5].
+  Proof. reflexivity. Qed.
+  Example test_app3 : [1, 2, 3] ++ nil = [1, 2, 3].
+  Proof. reflexivity. Qed.
+
+  Definition hd (default : nat) (l : natlist) : nat :=
+    match l with
+    | nil => default
+    | h :: t => h
+    end.
+
+  Definition tail (l : natlist) : natlist :=
+    match l with
+    | nil => nil
+    | h :: t => t
+    end.
+
+  Example test_hd1:             hd 0 [1,2,3] = 1.
+  Proof. reflexivity.  Qed.
+  Example test_hd2:             hd 0 [] = 0.
+  Proof. reflexivity.  Qed.
+  Example test_tail:            tail [1,2,3] = [2,3].
+  Proof. reflexivity.  Qed.
+
+  Fixpoint nonzeros (l:natlist) : natlist :=
+    match l with
+    | nil => nil
+    | h :: t => match h with
+                | O => nonzeros t
+                | _ => cons h (nonzeros t)
+                end
+    end.
+
+  Example test_nonzeros:            nonzeros [0,1,0,2,3,0,0] = [1,2,3].
+  Proof. reflexivity. Qed.
+
+  Fixpoint oddmembers (l:natlist) : natlist :=
+    match l with
+    | nil => nil
+    | h :: t => match oddb h with
+                | true => cons h (oddmembers t)
+                | false => oddmembers t
+                end
+    end.
+
+  Example test_oddmembers:            oddmembers [0,1,0,2,3,0,0] = [1,3].
+  Proof. reflexivity. Qed.
+
+  Fixpoint countoddmembers (l:natlist) : nat :=
+    match l with
+    | nil => O
+    | h :: t => match oddb h with
+                | true => S (countoddmembers t)
+                | false => countoddmembers t
+                end
+    end.
+
+  Example test_countoddmembers1:    countoddmembers [1,0,3,1,4,5] = 4.
+  Proof. reflexivity. Qed.
+  Example test_countoddmembers2:    countoddmembers [0,2,4] = 0.
+  Proof. reflexivity. Qed.
+  Example test_countoddmembers3:    countoddmembers nil = 0.
+  Proof. reflexivity. Qed.
