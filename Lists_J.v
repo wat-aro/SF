7af2176f3f600811cb70bd65d8a5e8895ef7d649
wat-aro <kazutas1008@gmail.com>
2018-11-11 17:55:22 +0900

    Finish Natlist

--- Lists_J.v
+++ Lists_J.v
@@ -1,55 +1,55 @@
 Require Export Basics_J.
 
 Module NatList.
-  Inductive natprod : Type :=
-    pair : nat -> nat -> natprod.
+  (* Inductive natprod : Type := *)
+  (*   pair : nat -> nat -> natprod. *)
 
-  Definition fst (p : natprod) : nat :=
-    match p with
-    | pair x y => x
-    end.
+  (* Definition fst (p : natprod) : nat := *)
+  (*   match p with *)
+  (*   | pair x y => x *)
+  (*   end. *)
 
-  Definition snd (p : natprod) : nat :=
-    match p with
-    | pair x y => y
-    end.
+  (* Definition snd (p : natprod) : nat := *)
+  (*   match p with *)
+  (*   | pair x y => y *)
+  (*   end. *)
 
-  Notation "( x , y )" := (pair x y).
+  (* Notation "( x , y )" := (pair x y). *)
 
-  Eval simpl in (fst (3, 4)).
+  (* Eval simpl in (fst (3, 4)). *)
 
-  Definition fst' (p : natprod) : nat :=
-    match p with
-    | (x, y) => x
-    end.
+  (* Definition fst' (p : natprod) : nat := *)
+  (*   match p with *)
+  (*   | (x, y) => x *)
+  (*   end. *)
 
-  Definition snd' (p : natprod) : nat :=
-    match p with
-    | (x, y) => y
-    end.
+  (* Definition snd' (p : natprod) : nat := *)
+  (*   match p with *)
+  (*   | (x, y) => y *)
+  (*   end. *)
 
-  Definition swap_pair (p : natprod) : natprod :=
-    match p with
-    | (x, y) => (y, x)
-    end.
+  (* Definition swap_pair (p : natprod) : natprod := *)
+  (*   match p with *)
+  (*   | (x, y) => (y, x) *)
+  (*   end. *)
 
-  Theorem surjective_pairing' : forall (n m : nat),
-      (n, m) = (fst (n, m), snd (n, m)).
-  Proof.
-    reflexivity.
-  Qed.
+  (* Theorem surjective_pairing' : forall (n m : nat), *)
+  (*     (n, m) = (fst (n, m), snd (n, m)). *)
+  (* Proof. *)
+  (*   reflexivity. *)
+  (* Qed. *)
 
-  Theorem subjective_pairing : forall (p : natprod),
-      p = (fst p, snd p).
-  Proof.
-    intros. destruct p as (n, m). simpl. reflexivity.
-  Qed.
+  (* Theorem subjective_pairing : forall (p : natprod), *)
+  (*     p = (fst p, snd p). *)
+  (* Proof. *)
+  (*   intros. destruct p as (n, m). simpl. reflexivity. *)
+  (* Qed. *)
 
-  Theorem snd_fst_is_swap : forall (p : natprod),
-      (snd p, fst p) = swap_pair p.
-  Proof.
-    intros p. destruct p as (n, m). simpl. reflexivity.
-  Qed.
+  (* Theorem snd_fst_is_swap : forall (p : natprod), *)
+  (*     (snd p, fst p) = swap_pair p. *)
+  (* Proof. *)
+  (*   intros p. destruct p as (n, m). simpl. reflexivity. *)
+  (* Qed. *)
 
   Inductive natlist : Type :=
   | nil : natlist
@@ -614,3 +614,4 @@ Module NatList.
         reflexivity.
         apply IHn'.
   Qed.
+End NatList.
