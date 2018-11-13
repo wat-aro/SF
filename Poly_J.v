f4691f86f9881ae24329f4038c107a72f539cd41
wat-aro <kazutas1008@gmail.com>
2018-11-13 17:46:33 +0900

    Finish trans_eq_exercise

--- Poly_J.v
+++ Poly_J.v
@@ -676,3 +676,34 @@ Proof.
           inversion H. rewrite <- H1. symmetry. apply Heqeqt.
         SCase "test x' = false".
           apply IHl' in H. apply H. Qed.
+
+Example trans_eq_example : forall (a b c d e f : nat),
+     [a,b] = [c,d] ->
+     [c,d] = [e,f] ->
+     [a,b] = [e,f].
+Proof.
+  intros a b c d e f eq1 eq2.
+  rewrite -> eq1. rewrite -> eq2. reflexivity.  Qed.
+
+Theorem trans_eq : forall {X:Type} (n m o : X),
+  n = m -> m = o -> n = o.
+Proof.
+  intros X n m o eq1 eq2. rewrite -> eq1. rewrite -> eq2.
+  reflexivity.  Qed.
+
+Example trans_eq_example' : forall (a b c d e f : nat),
+     [a,b] = [c,d] ->
+     [c,d] = [e,f] ->
+     [a,b] = [e,f].
+Proof.
+  intros a b c d e f eq1 eq2.
+
+  apply trans_eq with [c, d]. apply eq1. apply eq2. Qed.
+
+Example trans_eq_exercise : forall (n m o p : nat),
+    m = (minustwo o) ->
+    (n + p) = m ->
+    (n + p) = minustwo o.
+Proof.
+  intros n m o p eq1 eq2.
+  apply trans_eq with m. apply eq2. apply eq1. Qed.
